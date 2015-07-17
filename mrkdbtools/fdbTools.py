14f1d43a97da644be6d5e68e7c055d5c161a1e44
mateuszrajmundkukawski <mrkukawski@gmail.com>
2015-07-17 15:37:31 +0200

    new style SIGNAL

--- mrkdbtools/fdbTools.py
+++ mrkdbtools/fdbTools.py
@@ -2,36 +2,6 @@ import re
 import fdb
 
 
-def connecFirebird(dbase):
-    ''' return connection and curosr
-
-    must use two variables to get this values'''
-
-    con = fdb.connect(database=dbase, user='sysdba', password='masterkey', charset='ISO8859_2')
-    cur = con.cursor()
-    return con, cur
-
-def execute_select(SELECT, con, cur):
-
-    cur.execute(SELECT)
-    if re.search('^select', SELECT, re.IGNORECASE):
-        for row in cur:
-            print row
-    con.commit()
-    print 'commit'
-    
-def executeSqlFile(sql_file_name, con, cur):
-    
-    with open(sql_file_name) as f:
-        sql_query_list = (line.rstrip('\n') for line in f )
-        for row in sql_query_list:
-            execute_select(row, con, cur)
-
-def close_conn(con):
-    con.close()
-
-
-
 class DbTool(object):
     
     def __init__(self, dbName):
@@ -67,9 +37,6 @@ class DbTool(object):
 if __name__ == '__main__':
     test_db = '1815035.FDB'
     sqlfile = 'myquery.sql'
-    mycon, mycur = connecFirebird(test_db)
-    executeSqlFile(sqlfile, mycon, mycur)
-    close_conn(mycon)
     print '*'*10
     x = DbTool(test_db)
     x.dbConnection()
--- runApp.py
+++ runApp.py
@@ -1,11 +1,8 @@
 import sys, os
-from PyQt4 import QtCore, QtGui
+from PyQt4 import QtGui
 
-#from mrkdbtools.updateFDB_Ui import Ui_UpdateDatabase
-#from mrkdbtools.updateFDB_Ui import Ui_UpdateDatabase
 
 from mrkdbtools.updateFDB_Ui import Ui_UpdateDatabase
-import mrkdbtools.fdbTools as fdbt
 from  mrkdbtools.fdbTools import DbTool
 
 
@@ -17,59 +14,56 @@ class MyApp(QtGui.QMainWindow):
         self.ui = Ui_UpdateDatabase()
         self.ui.setupUi(self)
         self.my_init()
-        
+
     def my_init(self):
-                
+
         self.sql_file_name = None
-        QtCore.QObject.connect(self.ui.setDatabse_Button, QtCore.SIGNAL('clicked()'), self.getDBFileName)
-        QtCore.QObject.connect(self.ui.testSelectButton, QtCore.SIGNAL('clicked()'), self.validSelect)
-        QtCore.QObject.connect(self.ui.runButton, QtCore.SIGNAL('clicked()'), self.runUpdate)
-        QtCore.QObject.connect(self.ui.sqlFileButton, QtCore.SIGNAL('clicked()'), self.getSqlFileName)        
+        #QtCore.QObject.connect(self.ui.setDatabse_Button, QtCore.SIGNAL('clicked()'), self.getDBFileName)
+        self.ui.setDatabse_Button.clicked.connect(self.getDBFileName)
+
+        #QtCore.QObject.connect(self.ui.testSelectButton, QtCore.SIGNAL('clicked()'), self.validSelect)
+        self.ui.testSelectButton.clicked.connect(self.validSelect)
+        #QtCore.QObject.connect(self.ui.runButton, QtCore.SIGNAL('clicked()'), self.runUpdate)
+        self.ui.runButton.clicked.connect(self.runUpdate)
+        #QtCore.QObject.connect(self.ui.sqlFileButton, QtCore.SIGNAL('clicked()'), self.getSqlFileName)
+        self.ui.sqlFileButton.clicked.connect(self.getSqlFileName)
+
         self.ui.r1.setChecked(True)
 
-        
+
     def getDBFileName(self):
 
         self.full_file_db_path = QtGui.QFileDialog.getOpenFileName(self, 'Open DataBase', 'E:/', '*.fdb', )
         file_base_name = os.path.basename(str(self.full_file_db_path))
         old_lebel_text =  self.ui.database_name_Label.text()
         self.ui.database_name_Label.setText(old_lebel_text+file_base_name)
-        
+
     def getSqlFileName(self):
-        
+
         self.full_file_sql_path = QtGui.QFileDialog.getOpenFileName(self, 'Open SqlFile', 'E:/', '*.sql', )
         file_sql_base_name = os.path.basename(str(self.full_file_sql_path))
         old_lebel_text =  self.ui.sqlFileLabel.text()
         self.ui.sqlFileLabel.setText(old_lebel_text+file_sql_base_name)
 
     def validSelect(self):
-        
+
         self.SELECT = self.ui.select_text_edit.text()
         self.ui.selectLabel.setText(self.SELECT)
         print self.ui.r1.isChecked()
 
-    def runUpdate2(self):
-        
-        
-        fdbcon, fdbcur = fdbt.connecFirebird(str(self.full_file_db_path))
-        if self.ui.r1.isChecked():            
-            fdbt.execute_select(str(self.SELECT), fdbcon, fdbcur )
-        else:
-            fdbt.executeSqlFile(str(self.full_file_sql_path), fdbcon, fdbcur)
-        fdbt.close_conn(fdbcon)
-        
+
     def runUpdate(self):
-        
+
         work_fbd = DbTool(str(self.full_file_db_path))
         work_fbd.dbConnection()
-        
+
         if self.ui.r1.isChecked():
             work_fbd.execute_select(str(self.SELECT))
         elif self.ui.r2.isChecked():
             work_fbd.executeSqlFile(str(self.full_file_sql_path))
-            
+
         work_fbd.close_connection()
-        QtGui.QMessageBox.information(self,'Info', ':D')            
+        QtGui.QMessageBox.information(self,'Info', 'OK')
 
 
 
